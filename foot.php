a27a3de76aca8e521ee8a7d85346a6964e1ae492
Tagteam7 <35573304+Tagteam7@users.noreply.github.com>
2018-03-10 13:43:28 -0800

    Add files via upload

---
+++ foot.php
@@ -0,0 +1,100 @@
+ <footer> 
+ <div class="foot-light">
+            <div class="container">
+                <div class="row">
+                    <div class="span4">
+                        
+                    </div>
+                    <div class="span4">
+                       <br /><br /><br /><br /><br />             </div>
+                </div>
+            </div>
+        </div>
+    <div class="foot-dark">
+    <div class="container">
+                <div class="row">
+          <div class="span2 left-line-footer footer-container">
+            <h2>Billings</h2>
+            <i class="icon-map-marker iconleftpad"></i>
+            <p class="footer-location">1819 2nd Avenue North<br>
+              Billings, MT 59101<br>
+              Phone: 406-248-1151<br>
+              Fax: 406-252-8835</p>
+          </div>
+          <div class="span2 footer-container">
+            <h2>Missoula</h2>
+            <i class="icon-map-marker iconleftpad"></i>
+            <p class="footer-location">8989 Roller Coaster Road<br>
+              Missoula, MT 59808<br>
+              Phone: 406-543-2982<br>
+              Fax: 406-543-1982 </p>
+          </div>
+          <div class="span8">
+            <div class="pull-right footer-right-col">
+            </div>
+          </div>
+        </div>
+            </div>
+         </div>
+    <div class="foot-last">
+            <a href="#" id="toTheTop">
+                <span class="icon-chevron-up"></span>
+            </a>
+            <div class="container">
+                <div class="row">
+                    <div class="span6">&copy; <?php echo date("Y"); ?> Northwest Industrial Supplies, Inc.</div>
+                    <div class="span6">
+                        <div class="pull-right"><a href="#">Privacy Policy</a> | <a href="#">Terms of Service</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+  </footer>
+ 
+ <!--  ==========  --> 
+<!--  = JavaScript =  --> 
+<!--  ==========  --> 
+
+  <!--  = jQuery - CDN with local fallback =  --> 
+<script type="text/javascript" src="/assets/themes/nwimain/js/jquery.min.js"></script> 
+
+<!--  = _ =  --> 
+<script src="/assets/themes/nwimain/js/underscore/underscore-min.js" type="text/javascript"></script> 
+
+<!--  = Bootstrap =  --> 
+<script src="/assets/themes/nwimain/js/bootstrap.min.js" type="text/javascript"></script> 
+
+    <!--  = Slider Revolution =  -->
+    <script src="/assets/themes/nwimain/js/rs-plugin/js/jquery.themepunch.plugins.min.js" type="text/javascript"></script>
+    <script src="/assets/themes/nwimain/js/rs-plugin/js/jquery.themepunch.revolution.min.js" type="text/javascript"></script>
+
+    <!--  = CarouFredSel =  -->
+    <script src="/assets/themes/nwimain/js/jquery.carouFredSel-6.2.1-packed.js" type="text/javascript"></script>
+
+
+<!--  = jQuery UI =  --> 
+<script src="/assets/themes/nwimain/js/jquery-ui-1.10.3/js/jquery-ui-1.10.3.custom.min.js" type="text/javascript"></script> 
+<script src="/assets/themes/nwimain/js/jquery-ui-1.10.3/touch-fix.min.js" type="text/javascript"></script> 
+
+<!--  = Isotope =  --> 
+<script src="/assets/themes/nwimain/js/isotope/jquery.isotope.min.js" type="text/javascript"></script> 
+
+<!--  = Tour =  --> 
+<script src="/assets/themes/nwimain/js/bootstrap-tour/build/js/bootstrap-tour.min.js" type="text/javascript"></script>
+
+<!--  = PrettyPhoto =  --> 
+<script src="/assets/themes/nwimain/js/prettyphoto/js/jquery.prettyPhoto.js" type="text/javascript"></script> 
+
+    <!--  = Google Maps API =  -->
+    <!-- <script type="text/javascript" src="//maps.google.com/maps/api/js?sensor=false"></script> -->
+    <!-- <script type="text/javascript" src="/assets/themes/nwimain/js/goMap/js/jquery.gomap-1.3.2.min.js"></script> -->
+
+<!--  = Custom JS =  --> 
+<script src="/assets/themes/nwimain/js/custom.js" type="text/javascript"></script>
+<script src="/assets/js/nwi.js" type="text/javascript"></script>
+<?php
+  show_debug_log();
+  if ($debug_logging == "Y") {
+      show_d3debug_log();
+  }
+?>
---
+++ footer.php
@@ -0,0 +1,51 @@
+<!-- footer -->
+<div id="nwsi-footer" class="container-fluid margin-top100">
+	<div class="container">
+		<div class="row">
+			
+			<div class="col-xs-12 col-md-8 col-lg-6">
+				<ul id="footernav">									
+					<li>
+					<a href="index.php">Home</a></li>
+					<li>
+					<a href="online-tools.php">Online Tools</a></li> 
+					<li>
+					<a href="our-staff.php">Our Staff</a></li>
+					<li>
+					<a href="about-us.php">About Us</a></li>
+					<li>
+					<a target="_blank" href="assets/pdf/line_card.pdf">Line Card</a></li>
+				</ul>
+				<br>
+				<p class="copy-indent">&copy; 2016 Northwest Industrial, Inc.</p>
+				
+			</div>
+			
+			<div id="billoings-info" class="col-xs-12 col-md-3 col-lg-3">
+			<p><strong>Billings</strong><br>
+			1819 2nd Avenue N<br>
+			Billings, MT 59101<br>
+			Phone: 406-248-1151<br>
+			Fax: 406-252-8835 </p></div>
+			
+			<div id="missouls-info" class="col-xs-12 col-md-3 col-lg-3">
+			<p><strong>Missoula</strong><br>
+				8989 Roller Coaster Rd<br>
+				Missoula, MT 59808<br>
+				Phone: 406-543-2982<br>
+				Fax: 406-543-1982 </p></div>
+			
+		</div><!-- end row -->
+	</div><!-- end container -->
+</div><!-- end container-fluid -->
+<?php
+  show_debug_log();
+  if ($debug_logging == "Y") {
+      show_d3debug_log();
+  }
+?>
+</body>
+<script src="assets/plugins/jquery.min.js"></script>
+<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/plugins/impromptu/jquery-impromptu.min.js"></script>
+</html>
